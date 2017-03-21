82e904ced8407334ed254ba4474ec5ab9e8dac6d
dserero <daserboss@gmail.com>
2017-03-21 17:34:40 +0000

    Update texteditor.py

--- texteditor.py
+++ texteditor.py
@@ -5,40 +5,54 @@ os.chdir("/Users/yosemite/Documents/textswappingwork/")
 #changes to a particular directory
 #Todo: edit this so the program can be called anywhere
 
-#file1 = str(input('which file?'))
-file1 = "read.txt"
+#file1 = "read.txt"
+file1 = str(input('which file?'))
 #asks user this question, takes input and assigns it to the string variable 'file1'
+file2 = str(input('what is the name of the dictionnay?'))
 
 #Todo: have it ask which *file* to go to for the list of substitutions ... even better, allow going through folder tree
 #Or even better run this with parameters in commandline ... 
 #... >python texteditor.py oldfilename newfilename substitutionlistfilename (+ other parameter perhaps) 
 
-Old1 = str(input('What is your old word?'))
-New1 = str(input('What is your new word?'))
-Old2 = str(input('What is your old word?'))
-New2 = str(input('What is your new word?'))
-  
+mydictionnary = open(file2,'r')
+d={}
+# creates a dictionnary called d
+for line in mydictionnary:
+    x=line.split(',')
+    a = x[0]
+    b = x[1]
+    d[a]=b
+    # x is a string which contains the whole line
+    # x[0] is the first word of the line x[1] is the second
+    # d[a]=b appends in the dictionnary (d) the old and new word in the form d{'oldword' = newword}
+mydictionnary.close()
+
 thefile = open(file1,'r')
 #opens file1, read mode
 
 content = thefile.read()
 #copies everything in the file and assigns it to the string variable content
 
-content = content.replace(Old1 ,New1)
-content = content.replace(Old2 ,New2)
-#makes requested substitutions
+for k, v in d.items():
+    content = content.replace(k ,v)
+#makes requested substitutions using the dictionnary d
 
+#to change the existing file this is the code :
+'''
 thefile.close()
 #closes the file to allow edits
-
 thefile = open(file1,'w')
 #opens the file in the writing mode
-
 thefile.truncate()
 #clears all text from thefile
-
 thefile.write(content)
 #puts in the text from the string variable 'content'
-
 thefile.close()
 #need to close the file for some reason
+'''
+
+#to create a new file this is the code :
+mynewfile = open("newfile.txt",'w+')
+mynewfile.write(content)
+mynewfile.close()
+
