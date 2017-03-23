527d0823f8037322b6405fc4956f0744e11a37a5
daaronr <daaronr@gmail.com>
2017-03-23 11:48:12 +0000

    Revert "Update texteditor.py"
    This reverts commit e9f7460fb0322e1cbc73b79460c9068396b7aa50.
    
    # Conflicts:
    #	texteditor.py

--- texteditor.py
+++ texteditor.py
@@ -1,7 +1,7 @@
 import os
-#tells python to use the module 'os' 
+#tells python to use the module 'os'
 import sys
-#tells python to use the module 'sys' used to do one line command in the terminal 
+#tells python to use the module 'sys' used to do one line command in the terminal
 
 os.chdir("/Users/yosemite/Documents/textswappingwork/")
 #changes to a particular directory
@@ -12,6 +12,7 @@ arglist = sys.argv
 file1 = str(arglist[1])
 file2 = str(arglist[2])
 #takes as variable the elements of list 'arglist'
+
 """
 #file1 = "read.txt"
 file1 = str(input('which file?'))
@@ -19,22 +20,24 @@ file1 = str(input('which file?'))
 file2 = str(input('what is the name of the dictionnay?'))
 """
 
-#Todo: have it ask which *file* to go to for the list of substitutions ... even better, allow going through folder tree
-#Or even better run this with parameters in commandline ... 
-#... >python texteditor.py oldfilename newfilename substitutionlistfilename (+ other parameter perhaps) 
+#Or even better run this with parameters in commandline ...
+#... >python texteditor.py oldfilename newfilename substitutionlistfilename (+ other parameter perhaps)
 
 mydictionnary = open(file2,'r')
 d={}
-# creates a dictionnary called d
+# creates a dictionary called d
+
 for line in mydictionnary:
     x=line.split(',')
     a = x[0]
     b = x[1].strip()
     #no presentation errors eg. new line or extra spaces
+#DR: Later on we may want to allow quoted strings containing characters like newline
+
     d[a]=b
     # x is a string which contains the whole line
     # x[0] is the first word of the line x[1] is the second
-    # d[a]=b appends in the dictionnary (d) the old and new word in the form d{'oldword' = newword}
+    # d[a]=b appends in the dictionary (d) the old and new word in the form d{'oldword' = newword}
 mydictionnary.close()
 
 thefile = open(file1,'r')
@@ -45,9 +48,13 @@ content = thefile.read()
 
 for k, v in d.items():
     content = content.replace(k ,v)
-#makes requested substitutions using the dictionnary d
+#makes requested substitutions using the dictionary d
 
-#to change the existing file this is the code :
+#To change the existing file this is the code:
+
+#DR: For future work, let's allow a parameter to the command that specifies whether it edits in place or creates a new file;
+#... or even better, have it edit in place if the new and old filenames and locations are identical
+
 '''
 thefile.close()
 #closes the file to allow edits
@@ -59,6 +66,7 @@ thefile.write(content)
 #puts in the text from the string variable 'content'
 thefile.close()
 #need to close the file for some reason
+<<<<<<< Updated upstream
 '''
 
 #to create a new file this is the code :
